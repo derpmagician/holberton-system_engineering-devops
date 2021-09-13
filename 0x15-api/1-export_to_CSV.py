26e4c52bbd5e18aab2a6eef69fc82cc7b2a7685d
derpmagician <renzin@gmail.com>
2021-09-13 14:26:13 -0500

    [UPDATE] PEP8 fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Export data from an API to CSV format.
-"""
+"""Export data from an API to CSV format"""
 import csv
 import requests
 from sys import argv
@@ -14,3 +13,30 @@ if __name__ == '__main__':
     api = 'https://jsonplaceholder.typicode.com'
     user_api = '{}/users/{}'.format(api, employee_id)
     todos_user = '{}/todos'.format(user_api)
+    filename = '{}.csv'.format(employee_id)
+
+    # capture users
+    res_user = requests.get(user_api).json()
+
+    # capture tasks
+    res_todos = requests.get(todos_user).json()
+
+    # grab data of user
+    u_name = res_user.get('name')
+    u_username = res_user.get('username')
+
+    # grab number of tasks
+    n_tasks = len(res_todos)
+
+
+    with open(filename, 'w', encoding='utf-8') as csv_file:
+        my_writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+
+        for elem in res_todos:
+            # grab completion status
+            status = elem.get('completed')
+
+            # grab title
+            title = elem.get('title')
+
+            my_writer.writerow([employee_id, u_username, status, title])
