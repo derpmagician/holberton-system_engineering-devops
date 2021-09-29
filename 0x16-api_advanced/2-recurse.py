9c902768b8e05b40fb5530a742eb758369a5ba90
derpmagician <renzin@gmail.com>
2021-09-28 19:02:17 -0500

    [ADD] file

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,21 +5,27 @@ Queries the Reddit API
 import requests as rq
 
 
-def recurse(subreddit, hot_list=[], total=''):
+def recurse(subreddit, hot_list=[], after=None):
     """
     Returns a list with the titles of all hot articles of a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
 
     userAg = {'user-agent': 'derp'}
 
-    payload = {'limit': total}
+    payload = {'limit': after}
 
     res = rq.get(url, headers=userAg, params=payload, allow_redirects=False)
-    hot = res.json().get('data', None)
+    # hot = res.json().get('data', after)
 
     if res.status_code == 200:
-        for post in hot.get('children', []):
-            print(post.get('data', {}).get('title', ''))
+        top_posts = res.json()
+        key = top_posts['data']['after']
+        parent = top_posts['data']['children']
+        for obj in parent:
+            hot_list.append(obj['data']['title'])
+        if key is not None:
+            recurse(subreddit, hot_list, key)
+        return hot_list
     else:
-        return print('None')
+        return None
