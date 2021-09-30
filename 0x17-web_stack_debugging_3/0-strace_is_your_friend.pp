6c497a8178b488a4f0796ef6bdeed4c73d10bc23
derpmagician <renzin@gmail.com>
2021-09-29 22:48:35 -0500

    [UPDATE] erasing extra white space

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # This file fixes a typo in WP settings in a Holberton server
-
 exec { 'fix_wordpress_conf':
   provider => 'shell',
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin',
 }
-
