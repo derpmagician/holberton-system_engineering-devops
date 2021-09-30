bcd2975e66720f0e5ef7a03ba2bc3190bb8867b4
derpmagician <renzin@gmail.com>
2021-09-29 22:58:32 -0500

    [UPDATE] formating

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # This file fixes a typo in WP settings in a Holberton server
-exec { 'fix_wordpress_conf':
-  provider => 'shell',
+exec { 'fix wordpress conf':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin',
 }
